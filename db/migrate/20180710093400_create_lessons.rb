5993d16dee0e3bdb79dbc913a1bbf928564e8109
Paul A <paul.aguadisch@gmail.com>
2018-07-10 11:41:08 +0200

    rubocop -a pour gérer typo

--- db/migrate/20180710093400_create_lessons.rb
+++ db/migrate/20180710093400_create_lessons.rb
@@ -1,8 +1,8 @@
 class CreateLessons < ActiveRecord::Migration[5.2]
   def change
     create_table :lessons, id: :uuid do |t|
-    	t.string :title, null: false, limit:50
-    	t.text :description, limit: 300
+      t.string :title, null: false, limit: 50
+      t.text :description, limit: 300
 
       t.timestamps
     end
--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :lesson do
-    
   end
 end
